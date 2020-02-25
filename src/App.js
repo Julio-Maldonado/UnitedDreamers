1188b00405e6bf04b2c7ea087a1ce15f8d81b294
Julio-Maldonado <juliom@Julios-MacBook-Pro-3.local>
2020-02-24 21:37:36 -0800

    updating meta info

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Form from './views/Form';
 
 import { determineScreenState } from './views/Form/helperFunctions';
 
+import { Helmet } from 'react-helmet';
+
 import './App.css';
 
 class App extends React.Component {
@@ -28,6 +30,28 @@ class App extends React.Component {
 
     return (
       <div className="App">
+        {/* <Helmet>
+          <title>Yeux Technologies</title>
+          <meta
+              name="description"
+              content="Yeux have big goals and we want to help you achieve them. We design and develop websites, mobile apps, social media automations and any other techincal solution you might need. Our goal is to support small minority-owned business using our expertise in web development, mobile app development, logo designs, SEO, marketing, and social media. Let us help yeux."
+            />
+            <meta
+              property="og:title"
+              content="Yeux Technologies"
+            />
+            <meta
+              property="og:description"
+              content="Yeux have big goals and we want to help you achieve them. We design and develop websites, mobile apps, social media automations and any other techincal solution you might need. Our goal is to support small minority-owned business using our expertise in web development, mobile app development, logo designs, SEO, marketing, and social media. Let us help yeux."
+            />
+            <meta
+              property="og:image"
+              content="/logo192.png"
+            />
+            <meta property="og:url" content="yeux.tech"/>
+            <meta property="og:site_name" content="Yeux Technologies"/>
+            <meta property="og:type" content="website" />
+        </Helmet> */}
         <Form {...props}/>
       </div>
     );
